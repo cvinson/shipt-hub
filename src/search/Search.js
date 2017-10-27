6ca48b496056f5f1f95b491b3d57be1da69da673
Colin Vinson <cvinson86@gmail.com>
2017-10-27 15:14:42 -0500

    Added search button to Search component

--- src/search/Search.js
+++ src/search/Search.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import SearchIcon from 'material-ui/svg-icons/action/search';
 
 export default class Search extends React.Component {
   static props = {
@@ -24,9 +26,9 @@ export default class Search extends React.Component {
     }
   }
 
-  onChange = (event, text) => {
-    this.setState({ text });
-  }
+  onChange = (event, text) => this.setState({ text });
+
+  onClick = (event) => (this.state.text) && this.setState({ redirect: true });
 
   onKeyPress = (event) => {
     if (event.key === 'Enter' && this.state.text) {
@@ -43,6 +45,12 @@ export default class Search extends React.Component {
           onChange={this.onChange}
           onKeyPress={this.onKeyPress}
           value={this.state.text} />
+        <FlatButton
+          backgroundColor="#71b045"
+          hoverColor="#000000"
+          icon={<SearchIcon />}
+          onClick={this.onClick}
+          style={{ color: '#ffffff', marginLeft: '5px', minWidth: '40px' }} />
         {this.state.redirect &&
           <Redirect to={{ pathname: `/${this.state.text}`, state: { from: this.props.location } }} />
         }
